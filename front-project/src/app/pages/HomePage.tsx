c9da60f69a910e75a7f1d6bce4570bdbb036a5c7
Jaime mejia <jaimemejia1003@gmail.com>
2024-10-09 22:15:57 -0500

    examples

--- front-project/src/app/pages/HomePage.tsx
+++ front-project/src/app/pages/HomePage.tsx
@@ -1,9 +1,22 @@
+import { Button } from "@nextui-org/button"
+import { Calendar } from "@nextui-org/calendar"
+
 export const HomePage = () => {
     return (
         <div className="flex w-full flex-col">
 
             Esta es la pagina de inicio insana
             aca tiene que ir el contenido informativo
+
+            <Button>
+                Ejemplo
+            </Button>
+
+            <div className="flex gap-x-4">
+                eee
+                <Calendar aria-label="Date (No Selection)" />
+                <Calendar color="secondary" aria-label="Date (Uncontrolled)" />
+            </div>
         </div>
     )
 }
--- front-project/tailwind.config.js
+++ front-project/tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/components/(button|image|input|link|modal|navbar|table|ripple|spinner|checkbox|spacer).js"
+    "./node_modules/@nextui-org/theme/dist/components/(button|calendar|image|input|link|modal|navbar|table|ripple|spinner|checkbox|spacer).js"
   ],
   theme: {
     extend: {
