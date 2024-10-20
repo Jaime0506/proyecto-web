49303eab99e1294fb49fa85e4332aed44b9c10b4
Sebastian <sebastian.hernandorv@gmail.com>
2024-10-20 18:24:34 -0500

    Estilos a las card

--- front-project/src/app/pages/HomePage.tsx
+++ front-project/src/app/pages/HomePage.tsx
@@ -8,20 +8,21 @@ export const HomePage = () => {
   const { subjects } = useSubjects();
 
   const renderCards = () => {
-    // if(!subjects) return <></>
     return subjects?.map((subject, index) => (
       <Link to={`/app/student/schedule/${subject.name}`}>
-        <Card key={index} className="h-80 border-2">
+        <Card key={index} className="h-80 w-72 border-2 m-4 border-primary">
           <CardBody>
             <Image
-              alt="Card background"
+              alt={subject.name}
               src="https://concepto.de/wp-content/uploads/2014/08/programacion-2-e1551291144973.jpg"
               width={270}
               height={125}
             />
-            <h4 className="font-bold text-large pt-2">{subject.name}</h4>
+            <h4 className="font-bold text-xl pt-2 overflow-hidden">
+              {subject.name}
+            </h4>
           </CardBody>
-          <Divider className="mt-16" />
+            <Divider className="mt-64 border-primary absolute" />
         </Card>
       </Link>
     ));
@@ -29,9 +30,7 @@ export const HomePage = () => {
 
   return (
     <div className="flex w-full flex-col">
-      <Link to={"/app/student"}>Ir a student</Link>
-
-      <div className="flex flex-wrap gap-x-4 m-4">{renderCards()}</div>
+      <div className="flex flex-wrap gap-x-4 my-2 mx-20 ">{renderCards()}</div>
     </div>
   );
 };
