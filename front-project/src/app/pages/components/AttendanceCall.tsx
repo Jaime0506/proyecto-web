d8e61fdd7eaf2d0296fad6ee5aadaf061495f36f
Sebastian <sebastian.hernandorv@gmail.com>
2024-11-02 22:26:37 -0500

    Tabla de asistencia

--- front-project/src/app/pages/components/AttendanceCall.tsx
+++ front-project/src/app/pages/components/AttendanceCall.tsx
@@ -8,32 +8,53 @@ export const AttendanceCall = () => {
   const location = useLocation();
 
   const renderAttendance = () => {
-    return state?.map((attendance, index) => (
-      <TableRow key={index}>
-        <TableCell>{attendance.user_id}</TableCell>
-        <TableCell>{attendance.subject_id}</TableCell>
-        <TableCell>{attendance.attendance_id}</TableCell>
-      </TableRow>
-    )) || [];
+    return (
+      state?.map((attendance, index) => (
+        <TableRow key={index}>
+          <TableCell>{attendance.user_id}</TableCell>
+          <TableCell>{attendance.subject_id}</TableCell>
+          <TableCell>{attendance.attendance_id}</TableCell>
+          <TableCell>
+          <label >
+              <input 
+                type="radio" 
+                name={`attendance`} 
+                value="si" 
+              />
+            </label>
+            <label >
+              <input 
+                type="radio" 
+                name={`attendance`} 
+                value="no" 
+              />
+            </label>
+            <label >
+              <input 
+                type="radio" 
+                name={`attendance`} 
+                value="re" 
+              />
+            </label>
+          </TableCell>
+        </TableRow>
+      )) || []
+    );
   };
 
   const dato = location.state?.session;
   console.log(dato);
+
   return (
     <div className="flex flex-col gap-3">
-      <Table
-        color={"success"}
-        selectionMode="multiple"
-        aria-label="Example static collection table"
-      >
+      <Table color={"success"} aria-label="Example static collection table">
         <TableHeader>
           <TableColumn>user_id</TableColumn>
           <TableColumn>subject_id</TableColumn>
           <TableColumn>attendance_id</TableColumn>
+          <TableColumn>si no re</TableColumn>
         </TableHeader>
-        <TableBody>
-          {renderAttendance()}
-        </TableBody>
+        <TableBody>{renderAttendance()}</TableBody>
       </Table>
     </div>
   );
